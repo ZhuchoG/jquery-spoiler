9cdcf3c0c2057919e6160acef64b8baebd88484c
Vitaliy Zhukov <zhukov@el-machine.com>
2016-07-25 17:49:39 +0700

    change

--- jquery.spoiler.js
+++ jquery.spoiler.js
@@ -44,22 +44,8 @@
       var $this = $(this);
       // The only CSS requirement for this to work for the spoilered content
       // to have an overflow: hidden rule applied.
-      $this.css("overflow", "hidden");
-
-      // Get the height of the content to be spoilered now,
-      // as once we hide the text it cannot be restored.
-      // Use the value of `scrollHeight`, which does not change
-      // even if a height is applied through CSS.
-      var contentHeight = $this.prop("scrollHeight");
-
-      // Add padding to bottom of container only if enabled
-      contentHeight = (settings.includePadding ?
-                      contentHeight + parseInt(settings.paddingValue, 10) : contentHeight);
-      var spoiler  = $this.attr("data-spoiler-link");
-      spoilerHeights[spoiler] = contentHeight + "px";
-
       // Now that we have the height, hide all content
-      $this.css("height", "0");
+      $this.css("display", "none");
     });
 
 
@@ -70,8 +56,8 @@
           $content = $(contentClass + "[data-spoiler-link=" + spoiler + "]");
 
       // The container's collapsed/expanded height values
-      var showContent = {"height": spoilerHeights[spoiler]},
-          hideContent = {"height": "0"};
+      var showContent = {"display": "block"},
+          hideContent = {"display": "none"};
 
       // Check if content is visible or not
       var isVisible = $content.hasClass(settings.spoilerVisibleClass);
